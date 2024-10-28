e85c917d2a476445ff3882624b9b093532aadbdd
Acrto3Hil3 <subhashyadav98146@gmail.com>
2024-10-28 11:47:25 +0530

    first microservice program

---
+++ employee_service_oct25/src/main/java/com/utkarsh01/EmployeeServiceOct25Application.java
@@ -0,0 +1,13 @@
+package com.utkarsh01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServiceOct25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServiceOct25Application.class, args);
+	}
+
+}
