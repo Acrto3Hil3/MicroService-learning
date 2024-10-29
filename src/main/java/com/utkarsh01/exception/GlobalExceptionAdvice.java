234ab22e7da7ac2bd8a37df9db2594c8694feebe
Acrto3Hil3 <subhashyadav98146@gmail.com>
2024-10-29 23:51:24 +0530

    adding a home page

--- src/main/java/com/utkarsh01/exception/GlobalExceptionAdvice.java
+++ src/main/java/com/utkarsh01/exception/GlobalExceptionAdvice.java
@@ -1,11 +1,9 @@
 package com.utkarsh01.exception;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionAdvice {
-	@ExceptionHandler(value = EmployeeNotFoundException.class)
-	public ResponseEntity<app>
+	//@ExceptionHandler(value = EmployeeNotFoundException.class)
+	
 }
